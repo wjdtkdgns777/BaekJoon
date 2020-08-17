1a70eb824e2df50d392570ddda30cfbee587fa85
wjdtkdgns777 <60922290+wjdtkdgns777@users.noreply.github.com>
2020-08-17 21:13:27 +0900

    aa

---
+++ (BF)6064/(BF)6064/(BF)6064.cpp
@@ -0,0 +1,64 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int gcd(int a, int b)
+
+{
+
+    if (a % b == 0)
+
+        return b;
+
+
+
+    return gcd(b, a % b);
+
+}
+
+int lcm(int a, int b)
+
+{
+
+    return (a * b) / gcd(a, b);
+
+}
+
+
+int main()
+{      
+
+    int n;
+    cin >> n;
+    while (n--) {
+        int c, m, n, x, y;
+        cin >> m >> n>>x>>y;
+        c = lcm(m, n);
+
+        while (1) {
+            if (x == y) {
+
+                printf("%d\n", x);
+                break;
+
+            }
+
+            else if (x > y) {
+                y = y + n;
+            }
+            else if (y > x) {
+                x = x + m;
+            }
+
+            if ((x > c) || (y > c)) {
+                printf("-1");
+                break;
+            }
+        }
+
+       
+    }
+}

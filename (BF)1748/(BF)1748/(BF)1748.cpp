66d993146f5f86db26327ff0d3623c83133ada8b
wjdtkdgns777 <60922290+wjdtkdgns777@users.noreply.github.com>
2020-08-20 00:05:17 +0900

    s

---
+++ (BF)1748/(BF)1748/(BF)1748.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int a,count=0;
+    cin >> a;
+    for (int i = 1; i <= a; i++) {
+        int k;
+        k = i;
+        while (k > 0) {
+            count++;
+            k = k / 10;
+        }
+
+
+    }
+    printf("%d", count);
+}
+

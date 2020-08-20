003d7a470754a376faaa1974d7eee460008c4cb8
wjdtkdgns777 <60922290+wjdtkdgns777@users.noreply.github.com>
2020-08-20 20:39:54 +0900

    https://www.acmicpc.net/problem/2579

---
+++ (DP)2579/(DP)2579/(DP)2579.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    int ss[301];
+    int score[301];
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        cin >> ss[i];
+    }
+    score[0] = 0;
+    score[1] = ss[1];
+    score[2] = ss[1] + ss[2];
+
+    for (i = 3; i <= n; i++) {
+        score[i] = max(ss[i - 1] + score[i - 3] + ss[i], score[i - 2] + ss[i]);
+    }
+
+    printf("%d", score[n]);
+}

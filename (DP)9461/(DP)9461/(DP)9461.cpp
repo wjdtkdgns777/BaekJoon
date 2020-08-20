3fb950885cad6ffbb32e4fd9d88174c940dd62da
wjdtkdgns777 <60922290+wjdtkdgns777@users.noreply.github.com>
2020-08-20 21:08:45 +0900

    https://www.acmicpc.net/problem/9461

---
+++ (DP)9461/(DP)9461/(DP)9461.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    long long ss[101];
+    cin >> n;
+    ss[1] = 1;
+    ss[2] = 1;
+    ss[3] = 1;
+    ss[4] = 2;
+    ss[5] = 2;
+    ss[6] = 3;
+    ss[7] = 4;
+    ss[8] = 5;
+    ss[9] = 7;
+    ss[10] = 9;
+    for (int i = 11; i <= 100; i++) {
+        ss[i] = ss[i - 1] + ss[i - 5];
+
+
+    }
+
+    while (n--) {
+        int a;
+        cin >> a;
+        printf("%lld\n", ss[a]);
+    }
+}
+

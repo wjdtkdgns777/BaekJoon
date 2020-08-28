ac754d7806333ed59c1bf006299190026a3b0c5f
wjdtkdgns777 <60922290+wjdtkdgns777@users.noreply.github.com>
2020-08-28 21:50:06 +0900

    a

--- (DP)2011/(DP)2011/(DP)2011.cpp
+++ (DP)2011/(DP)2011/(DP)2011.cpp
@@ -6,11 +6,11 @@ int main()
 	char a[5002];
 	scanf("%s", a + 1);
 	int n = strlen(a + 1);
-	for (int i = 1; i <= n; i++)
-	{
+	int i = 1;
+	
 		int x = a[i] - '0', y = a[i - 1] - '0';
 		if (1 <= x && x <= 9) d[i] =  d[i - 1] % 1000000;
 		if (10 <= 10 * y + x && 10 * y + x <= 26) d[i] = (d[i] + d[i - 2]) % 1000000;
-	}
-	printf("%d", d[n]);
+	
+	printf("%d",y);
 }
---
+++ 2751/2751/2751.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int num;
+    
+    cin >> num;
+    vector<int> a(num);
+    for (int i = 0; i < num; i++)
+    {
+        cin >> a[i];
+       
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < num; i++)
+        cout << a[i] << '\n';
+}

e0556b4f5db1d01864feed8da6453494c8b20a89
wjdtkdgns777 <60922290+wjdtkdgns777@users.noreply.github.com>
2020-08-28 21:22:34 +0900

    https://www.acmicpc.net/problem/2011

---
+++ (DP)2011/(DP)2011/(DP)2011.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int d[5001] = { 1, };
+	char a[5002];
+	scanf("%s", a + 1);
+	int n = strlen(a + 1);
+	for (int i = 1; i <= n; i++)
+	{
+		int x = a[i] - '0', y = a[i - 1] - '0';
+		if (1 <= x && x <= 9) d[i] =  d[i - 1] % 1000000;
+		if (10 <= 10 * y + x && 10 * y + x <= 26) d[i] = (d[i] + d[i - 2]) % 1000000;
+	}
+	printf("%d", d[n]);
+}

91b8ac1a8c525ab7f569a4618ff3d275c63863d1
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-01-09 17:39:16 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,13 @@
+N,M =map(int,input().split())
+
+A = [list(map(int,input().split())) for _ in range(N)]
+B = [list(map(int,input().split())) for _ in range(N)] 
+
+C=[[0]*M for _ in range(N)]
+
+for i in range(N):
+  for j in range(M):
+    C[i][j]=A[i][j]+B[i][j]
+
+for row in C:
+  print(' '.join(map(str,row)))

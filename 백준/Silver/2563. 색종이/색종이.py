423a82521acc8e5d187315fc6d6a7495a3687f0a
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-01-10 17:28:50 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,16 +1,10 @@
-tile = [[0]*100 for _ in range(100)]
-sum =0
+tile = [[0] * 100 for _ in range(100)]
 N = int(input())
 for _ in range(N):
-  x,y = map(int,input().split())
-  for i in range(x,x+10):
-    for j in range (y,y+10):
-      if (i<100 and j<100):
-        tile[i][j] = 1
+    x, y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            tile[i][j] = 1
 
-for i in range(100):
-  for j in range(100):
-    if(tile[i][j]==1):
-      sum=sum+1
-
-print(sum)
+total_area = sum(sum(row) for row in tile)
+print(total_area)

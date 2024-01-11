79ba3e0643096dd9e5ccac9ff9bdbe3988d8d377
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-01-11 15:36:14 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,9 +1,9 @@
 N = int(input())
-level = 1  # 중앙의 1번 방부터 시작
-max_number_in_level = 1  # 현재 레벨에서의 최대 방 번호
+max = 1
+level = 1
 
-while N > max_number_in_level:
-    max_number_in_level += 6 * level  # 다음 레벨로 이동할 때마다 6의 배수만큼 증가
-    level += 1  # 레벨 증가
+while(N>max):
+  max = max+6*level
+  level = level+1
 
 print(level)

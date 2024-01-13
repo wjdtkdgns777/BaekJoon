362a0431a664e1559fe1188b970c9240ad93eeda
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-01-13 15:40:01 +0900

    [Silver V] Title: 셀프 넘버, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,11 +1,10 @@
-num = set(range(1,10001))
-gen = set()
+constructor_number = [0]*10001
 
-for i in range (1,10001):
-  for j in str(i):
-    i+=int(j)
-  gen.add(i)
+for i in range(1, 10001):
+  sum_number = sum(map(int,str(i)))
+  if (sum_number+i)<10001:
+    constructor_number[sum_number+i] = 1
 
-self = sorted(num-gen)
-for i in self:
-  print(i)
+for i in range(1, 10001):
+  if constructor_number[i]==0:
+    print(i)

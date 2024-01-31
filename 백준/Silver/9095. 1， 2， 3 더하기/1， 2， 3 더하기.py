c2f80762eef0367d8d659e37fc718fcbd35fb1cb
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-01-31 16:58:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,15 +1,9 @@
-import sys
-
-n = int(input())
-
-arr = [0] * 11
-arr[1] = 1
-arr[2] = 2
-arr[3] = 4
+N = int(input())
+dp = [0,1,2,4]+[0]*7
 
 for i in range(4,11):
-    arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+  dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
 
-for i in range(0,n):
-    testNum = int(input())
-    print(arr[testNum])
+for _ in range(N):
+  n = int(input())
+  print(dp[n])

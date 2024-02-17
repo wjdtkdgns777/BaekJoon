2a64cbfcea5f0eff24f32d2e9e1a083916b6e856
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-02-17 17:09:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 328 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,26 +1,26 @@
-while True:
-    words = input()
-    if words == '.':
-        break
-
+def is_valid(N):
     stack = []
-    is_valid = True
-
-    for word in words:
-        if word in '([':
-            stack.append(word)
-        elif word == ')':
-            if not stack or stack[-1] == '[':
-                is_valid = False
-                break
-            stack.pop()
-        elif word == ']':
-            if not stack or stack[-1] == '(':
-                is_valid = False
-                break
-            stack.pop()
-
-    if is_valid and not stack:
-        print("yes")
+    for i in N:
+        if i in '([':
+            stack.append(i)
+        elif i == ')':
+            if not stack or stack[-1] != '(':  # 스택이 비었거나 마지막 요소가 '('가 아니면
+                return False
+            else:
+                stack.pop()
+        elif i == ']':
+            if not stack or stack[-1] != '[':  # 스택이 비었거나 마지막 요소가 '['가 아니면
+                return False
+            else:
+                stack.pop()
+    
+    if stack:  # 스택에 남아 있는 요소가 있으면 균형이 맞지 않음
+        return False
     else:
-        print("no")
+        return True
+
+while True:
+    N = input()
+    if N == '.':
+        break
+    print("yes" if is_valid(N) else "no")

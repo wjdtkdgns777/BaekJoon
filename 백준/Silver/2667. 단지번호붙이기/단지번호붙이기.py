70b823963cb184aa9e04be18eb9ef54a7de4dfef
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-02-19 22:39:58 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,34 @@
+n = int(input())
+graph = [list(map(int, input())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+# 상하좌우 이동을 위한 방향 벡터 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y):
+    global cnt
+    visited[x][y] = True
+    cnt += 1
+    # 상하좌우 위치 확인
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 지도 범위 내에서
+        if 0 <= nx < n and 0 <= ny < n:
+            # 연결된 집이 있고 아직 방문하지 않았다면
+            if graph[nx][ny] == 1 and not visited[nx][ny]:
+                dfs(nx, ny)
 
 cnt = 0
-n = int(input())
-s=[]
-apt = []
-
-dx = [0,0,-1,1]
-dy=[1,-1,0,0]
-
-for i in range(n):
-  x = list(map(int,input()))
-  s.append(x)
-
-def dfs(x,y):
-  global cnt
-  cnt = cnt + 1
-  s[x][y]=0
-  for i in range(4):
-    if(0<=x+dx[i]<n and 0<= y+dy[i]<n):
-      if(s[x+dx[i]][y+dy[i]]==1):
-        dfs(x+dx[i],y+dy[i])
-
-
+result = []
 for i in range(n):
     for j in range(n):
-        if s[i][j] == 1:
-            cnt= 0
-            dfs(i,j)
-            apt.append(cnt)
+        if graph[i][j] == 1 and not visited[i][j]:
+            cnt = 0
+            dfs(i, j)
+            result.append(cnt)
 
-
-print(len(apt))
-apt.sort()
-for i in apt:
-    print(i)
+print(len(result))
+for r in sorted(result):
+    print(r)

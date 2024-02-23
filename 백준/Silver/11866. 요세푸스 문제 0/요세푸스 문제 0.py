476273081f28af777638dcb660707d4209db73a9
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-02-23 23:49:45 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 68 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,15 +1,9 @@
 from collections import deque
-
 N,K = map(int,input().split())
-first_que = deque()
-ans_que = deque()
+list = []
+q = deque(range(1,N+1))
+while q:
+  q.rotate(-K+1)
+  list.append(q.popleft())
 
-for i in range(1,N+1):
-  first_que.append(i)
-
-while first_que:
-  for i in range(K-1):
-    first_que.append(first_que.popleft())
-  ans_que.append(first_que.popleft())
-
-print("<", ", ".join(map(str, ans_que)), ">", sep="")
+print("<" + ", ".join(map(str,list))+">")

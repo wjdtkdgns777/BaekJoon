40bd642b2ffe0c2e2f116a1919bcafc53883146f
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-03-17 23:53:56 +0900

    [Silver III] Title: 숫자 정사각형, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -0,0 +1,33 @@
+N, M = map(int,input().split())
+A = [[] for _ in range(N)]
+
+for i in range(N):
+    A[i] = list(map(int,input()))
+
+ans = []
+
+if N >= M:
+    size = M
+else:
+    size = N
+
+i = 0
+j = 0
+
+while size > 0:
+    
+    if A[i][j] == A[i+size-1][j] == A[i][j+size-1] == A[i+size-1][j+size-1]:
+        ans.append(size**2)
+    
+    j += 1
+    
+    if j+size-1 >= M:
+        j = 0
+        i += 1
+        
+    if i+size-1 >= N:
+        size -= 1
+        i = 0
+        j = 0
+        
+print(max(ans))

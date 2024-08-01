27f41bb6ef89309965df5f54e3be9436d2a9c13d
Jeong Sang Hoon <60922290+wjdtkdgns777@users.noreply.github.com>
2024-08-01 23:44:05 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(input())
+cnt = 0
+cow = [-1] * 11
+for _ in range(n):
+	a,b = map(int,sys.stdin.readline().split())
+	if cow[a] == -1:
+		cow[a] = b
+	elif cow[a] != b:
+		cow[a] = b
+		cnt+=1
+print(cnt)
